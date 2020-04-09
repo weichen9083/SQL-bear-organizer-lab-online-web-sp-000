4949df22e80667829e71f855376e60cd8f789271
wei chen <weichen9083@gmail.com>
2020-04-09 03:03:53 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO bears (name, age, gender, color,temperament, alive) VALUES ("Mr. Chocolate", 2, "male","black", "nice", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament, alive) VALUES ("Rowdy", 7, "male","brown", "mean", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament, alive) VALUES ("Tabitha", 3, "female","white", "cute", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament, alive) VALUES ("Sergeant Brown", 10, "male","brown", "sexy", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament, alive) VALUES ("Melissa", 1, "female","red", "cute", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament, alive) VALUES ("Grinch", 2, "male","green", "aggressive", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament, alive) VALUES ("Wendy", 6, "female","black", "pretty", 1);
+
+INSERT INTO bears (name, age, gender, color,temperament, alive) VALUES (NULL, 2, "male","black", "super mean", 0);
